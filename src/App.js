68d1033b7dc9151e5483d38079b1dee9b801527b
thi131190 <thi131190@gmail.com>
2019-10-24 14:34:21 +0700

    Edit README & Implement Github OAuth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  const clientId = process.env.CLIENT_ID
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const existingToken = sessionStorage.getItem('token')
+    const accessToken =
+      window.location.search.split('=')[0] === '?access_token'
+        ? window.location.search.split('=')[1]
+        : null
+
+    if (!accessToken && !existingToken) {
+      window.location.replace(
+        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
+      )
+    }
+
+    if (accessToken) {
+      console.log(`New accessToken: ${accessToken}`)
+
+      sessionStorage.setItem('token', accessToken)
+      setToken(accessToken)
+    }
+
+    if (existingToken) {
+      setToken(existingToken)
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  console.log(token)
+
+  return <div className='App'>Hwheheheh</div>
 }
 
-export default App;
+export default App
