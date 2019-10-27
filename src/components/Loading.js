9cbea4a9f60bb6a36a1ea5381238943fa350944f
Edward Stevens <edwardstevens@Edwards-MacBook-Pro.local>
2019-10-27 21:44:26 +0700

    updated to loader and logo on navbar prior to getting new master

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Loading() {
+    return (
+       
+    )
+}
